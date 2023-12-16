9d06b6e15e2f1e9601ef62a9502a7b940aef0030
Alexandru Sima <alexandru.sima1512@stud.acs.upb.ro>
2023-12-16 12:11:10 +0200

    Merge primul testgit status

--- src/ipc.c
+++ src/ipc.c
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
+#include <netinet/in.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
 
 #include "ipc.h"
 
+#define PORT 5432
 
 int create_socket(void)
 {
-	/* TODO: Implement create_socket(). */
-	int ret = socket(AF_LOCAL ,SOCK_STREAM, 0);
+	int ret = socket(AF_UNIX, SOCK_STREAM, 0);
 	if (ret == -1) {
 		perror("socket");
 		exit(-1);
@@ -24,23 +25,54 @@ int create_socket(void)
 
 int connect_socket(int fd)
 {
-	/* TODO: Implement connect_socket(). */
-	return -1;
+	struct sockaddr_un sockaddr;
+	memset(&sockaddr, 0, sizeof(sockaddr));
+	sockaddr.sun_family = AF_UNIX;
+	strncpy(sockaddr.sun_path, SOCKET_NAME, sizeof(sockaddr.sun_path) - 1);
+
+	int ret = connect(fd, (struct sockaddr *)&sockaddr, sizeof(sockaddr));
+	return ret;
 }
 
 ssize_t send_socket(int fd, const char *buf, size_t len)
 {
-	/* TODO: Implement send_socket(). */
-	return -1;
+	size_t sent = 0;
+	while (sent < len) {
+		int ret = send(fd, buf + sent, len - sent, 0);
+
+		if (ret == -1) {
+			perror("send");
+			exit(-1);
+		}
+
+		sent += ret;
+	}
+	printf("Sent: %s\n", buf);
+	return sent;
 }
 
 ssize_t recv_socket(int fd, char *buf, size_t len)
 {
-	/* TODO: Implement recv_socket(). */
-	return -1;
+	size_t received = 0;
+
+	int ret = read(fd, buf, len);
+
+	if (ret < 0) {
+		perror("recv");
+		exit(-1);
+	}
+
+	received += ret;
+
+	printf("Received: %s\n", buf);
+	return received;
 }
 
 void close_socket(int fd)
 {
-	/* TODO: Implement close_socket(). */
+	int ret = close(fd);
+	if (ret == -1) {
+		perror("close");
+		exit(-1);
+	}
 }
--- src/server.c
+++ src/server.c
@@ -5,8 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/un.h>
 #include <unistd.h>
 
 #include "ipc.h"
@@ -18,31 +20,68 @@
 
 static int lib_prehooks(struct lib *lib)
 {
-	/* TODO: Implement lib_prehooks(). */
+	/* Create outputfile name. */
+	lib->outputfile = malloc(sizeof(OUTPUT_TEMPLATE));
+	if (!lib->outputfile) {
+		perror("malloc");
+		return -1;
+	}
+	strncpy(lib->outputfile, OUTPUT_TEMPLATE, sizeof(OUTPUT_TEMPLATE));
+
+	/* Create outputfile. */
+	int fd = mkstemp(lib->outputfile);
+	if (fd == -1) {
+		perror("mkstemp");
+		return -1;
+	}
+
+	printf("outputfile: %s\n", lib->outputfile);
 	return 0;
 }
 
 static int lib_load(struct lib *lib)
 {
-	/* TODO: Implement lib_load(). */
+	lib->handle = dlopen(lib->libname, RTLD_LAZY);
+	if (!lib->handle) {
+		perror("dlopen");
+		return -1;
+	}
+
+	printf("libname: %s\n", lib->libname);
+
+	void *addr = dlsym(lib->handle, lib->funcname);
+	if (!addr) {
+		fprintf(stderr, "%s\n", dlerror());
+		return -1;
+	}
+
+	if (lib->filename) {
+		lib->p_run = addr;
+	} else {
+		lib->run = addr;
+	}
 	return 0;
 }
 
 static int lib_execute(struct lib *lib)
 {
-	/* TODO: Implement lib_execute(). */
+	if (lib->filename) {
+		lib->p_run(lib->filename);
+	} else {
+		lib->run();
+	}
+
 	return 0;
 }
 
 static int lib_close(struct lib *lib)
 {
-	/* TODO: Implement lib_close(). */
+	dlclose(lib->handle);
 	return 0;
 }
 
 static int lib_posthooks(struct lib *lib)
 {
-	/* TODO: Implement lib_posthooks(). */
 	return 0;
 }
 
@@ -82,16 +121,82 @@ static int parse_command(const char *buf, char *name, char *func, char *params)
 
 int main(void)
 {
-	/* TODO: Implement server connection. */
-	int ret;
+	int opt = 1;
+	int fd = create_socket();
+
+	/* Initialize socket address. */
+	struct sockaddr_un addr;
+	socklen_t addrlen = sizeof(addr);
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	strncpy(addr.sun_path, SOCKET_NAME, sizeof(addr.sun_path) - 1);
+
+	// if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+	// 	perror("setsockopt");
+	// 	exit(EXIT_FAILURE);
+	// }
+
+	int ret = bind(fd, (struct sockaddr *)&addr, sizeof(addr));
+	if (ret == -1) {
+		perror("bind");
+		exit(-1);
+	}
+
+	if (listen(fd, MAX_CLIENTS) < 0) {
+		perror("listen");
+		exit(-1);
+	}
+
+	int sockfd = accept(fd, (struct sockaddr *)&addr, &addrlen);
+	if (sockfd == -1) {
+		perror("accept");
+		exit(-1);
+	}
+
+	printf("accepted\n");
+
+	char buf[BUFSIZE] = {0};
 	struct lib lib;
 
 	while (1) {
+		if (recv_socket(sockfd, buf, BUFSIZE) < 0) {
+			perror("recv err");
+			exit(-1);
+		}
+
+		char name[BUFSIZ] = {0};
+		char func[BUFSIZ] = {0};
+		char params[BUFSIZ] = {0};
+
+		parse_command(buf, name, func, params);
+		printf("buf: %s\n", buf);
+		printf("func: %p\n", func);
+		printf("name: %s\n", name);
+
+		if (strlen(func) == 0) {
+			strcpy(func, "run");
+		}
+
+		struct lib lib = {
+			.libname = name, .funcname = func, .filename = params};
+
 		/* TODO - get message from client */
 		/* TODO - parse message with parse_command and populate lib */
 		/* TODO - handle request from client */
-		ret = lib_run(&lib);
+		if (lib_run(&lib) < 0) {
+			perror("run err");
+			exit(-1);
+		}
+
+		printf("outputfile: %s\n", lib.outputfile);
+		if (send_socket(sockfd, lib.outputfile, strlen(lib.outputfile)) < 0) {
+			perror("send err");
+			exit(-1);
+		}
 	}
+
+	close_socket(sockfd);
+	close_socket(fd);
 
 	return 0;
 }
